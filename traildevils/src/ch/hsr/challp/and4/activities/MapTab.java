61a509ead5d8f88cc27c1d22bb6bea34e2846819
ggrassi <ggrassi@hsr.ch>
2011-10-10 23:21:58 +0200

    maptest

--- traildevils/src/ch/hsr/challp/and4/activities/MapTab.java
+++ traildevils/src/ch/hsr/challp/and4/activities/MapTab.java
@@ -1,16 +1,41 @@
 package ch.hsr.challp.and4.activities;
 
-import android.app.Activity;
 import android.os.Bundle;
+import android.widget.LinearLayout;
+import ch.hsr.challp.and4.R;
 
-public class MapTab extends Activity {
-	/** Called when the activity is first created. */
-	@Override
+import com.google.android.maps.MapActivity;
+import com.google.android.maps.MapController;
+import com.google.android.maps.MapView;
+import com.google.android.maps.MyLocationOverlay;
+
+public class MapTab extends MapActivity {
+	private long geoKontaktId;
+	
+//	private GeoKontakt kontakt;
+	private LinearLayout linLayout;
+	private MapView mapView;
+	private MapController mapCtrl;
+	private MyLocationOverlay locationOverlay;
+//	private GeoKontactOverlay mapViewOverlay;
+	
+	
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-//		setContentView(ch.hsr.challp.and4.R.layout.list_view);
-
+		setContentView(R.layout.map);
 		
+		initMapView();
+	}
+	
+	private void initMapView() {
+		mapView = (MapView) findViewById(R.id.mapview);
+		mapView.setBuiltInZoomControls(true);
+		mapView.setSatellite(true);
+	
+	}
 
+	@Override
+	protected boolean isRouteDisplayed() {
+		return false;
 	}
 }
