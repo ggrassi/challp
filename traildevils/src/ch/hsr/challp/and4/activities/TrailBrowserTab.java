7f37b63916c0c29f02d839acb0ac962b1f79481a
Michael Schmuki <mschmuki@hsr.ch>
2011-12-12 16:41:06 +0100

    Search Implemented

--- traildevils/src/ch/hsr/challp/and4/activities/TrailBrowserTab.java
+++ traildevils/src/ch/hsr/challp/and4/activities/TrailBrowserTab.java
@@ -6,6 +6,7 @@ import android.app.ListActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ListView;
@@ -14,9 +15,7 @@ import ch.hsr.challp.and4.adapter.BrowserListAdapter;
 import ch.hsr.challp.and4.domain.Trail;
 
 public class TrailBrowserTab extends ListActivity {
-	private BrowserListAdapter t_adapter = null;
 
-	/** Called when the activity is first created. */
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -24,9 +23,7 @@ public class TrailBrowserTab extends ListActivity {
 
 		ArrayList<Trail> trails = Trail.getTrails();
 
-		this.t_adapter = new BrowserListAdapter(this, R.layout.list_entry,
-				trails);
-		setListAdapter(this.t_adapter);
+		setListAdapter(new BrowserListAdapter(this, R.layout.list_entry, trails));
 
 		ListView lv = getListView();
 		lv.setTextFilterEnabled(true);
@@ -36,9 +33,19 @@ public class TrailBrowserTab extends ListActivity {
 					int position, long id) {
 
 				Intent ac = new Intent(".activities.TrailDetail");
-				ac.putExtra("key", t_adapter.getItem(position).getTrailId());
+
+				ac.putExtra("key",
+						((Trail) parent.getAdapter().getItem(position))
+								.getTrailId());
 				startActivity(ac);
 			}
 		});
+	}
+
+	@Override
+	public boolean onSearchRequested() {
+		getWindow().setSoftInputMode(
+				WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_VISIBLE);
+		return false;
 	}
 }
--- traildevils/src/ch/hsr/challp/and4/adapter/TrailListAdapter.java
+++ traildevils/src/ch/hsr/challp/and4/adapter/TrailListAdapter.java
@@ -22,6 +22,8 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.Filter;
+import android.widget.Filterable;
 import android.widget.ImageView;
 import android.widget.TextView;
 import ch.hsr.challp.and.R;
@@ -29,17 +31,14 @@ import ch.hsr.challp.and4.domain.Trail;
 import ch.hsr.challp.and4.technicalservices.UserLocationListener;
 
 public abstract class TrailListAdapter extends ArrayAdapter<Trail> implements
-		Observer {
+		Observer, Filterable {
+
 	protected ArrayList<Trail> trails;
-	private Context context;
-	private View v;
-	private Trail trail;
-	private ImageView listImg;
+	private TrailFilter filter;
 
 	public TrailListAdapter(Context context, int textViewResourceId,
 			ArrayList<Trail> trails) {
 		super(context, textViewResourceId, trails);
-		this.context = context;
 		this.trails = trails;
 		UserLocationListener.getInstance().addObserver(this);
 	}
@@ -47,20 +46,17 @@ public abstract class TrailListAdapter extends ArrayAdapter<Trail> implements
 	public abstract void update(Observable observable, Object data);
 
 	@Override
-	public View getView(int position, View convertView,
-			ViewGroup parent) {
-		v = convertView;
+	public View getView(int position, View convertView, ViewGroup parent) {
+		View v = convertView;
 		if (v == null) {
-			LayoutInflater vi = (LayoutInflater) context
+			LayoutInflater vi = (LayoutInflater) getContext()
 					.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
 			v = vi.inflate(R.layout.list_entry, null);
 		}
 
-		trail = trails.get(position);
+		Trail trail = trails.get(position);
 
 		if (trail != null) {
-			listImg = (ImageView) v.findViewById(R.id.status_icon);
-
 			TextView parkname = (TextView) v.findViewById(R.id.list_parkname);
 			TextView info = (TextView) v.findViewById(R.id.list_info);
 			TextView date = (TextView) v.findViewById(R.id.list_date);
@@ -72,10 +68,11 @@ public abstract class TrailListAdapter extends ArrayAdapter<Trail> implements
 			}
 		}
 		try {
-			setTrailIcon();
+			setTrailIcon(trail, v);
 		} catch (Exception e) {
 			Log.d("tag", "filtrino: " + e.toString() + "");
 		}
+		v.setId(trail.getTrailId());
 		return v;
 	}
 
@@ -105,9 +102,9 @@ public abstract class TrailListAdapter extends ArrayAdapter<Trail> implements
 		return distance.toString();
 	}
 
-	public void setTrailIcon() {
+	public void setTrailIcon(Trail trail, View v) {
 		try {
-			loadImage();
+			loadImage(trail, (ImageView) v.findViewById(R.id.status_icon));
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
@@ -115,9 +112,9 @@ public abstract class TrailListAdapter extends ArrayAdapter<Trail> implements
 		iconContainer.setVisibility(View.VISIBLE);
 	}
 
-	private void loadImage() throws Exception {
+	private void loadImage(Trail trail, ImageView listImg) throws Exception {
 		Drawable trailDraw = null;
-		File imageFile = new File(context.getFilesDir(), String.valueOf(trail
+		File imageFile = new File(getContext().getFilesDir(), String.valueOf(trail
 				.getTrailId()) + "_120.png");
 		if (imageFile.exists()) {
 			trailDraw = new BitmapDrawable(BitmapFactory.decodeFile(imageFile
@@ -128,7 +125,7 @@ public abstract class TrailListAdapter extends ArrayAdapter<Trail> implements
 				InputStream is = (InputStream) new URL(trail.getImageUrl120())
 						.getContent();
 				trailDraw = Drawable.createFromStream(is, "src name");
-				saveDownloadedFile(trailDraw);
+				saveDownloadedFile(trail, trailDraw);
 			}
 		}
 
@@ -140,10 +137,10 @@ public abstract class TrailListAdapter extends ArrayAdapter<Trail> implements
 
 	}
 
-	private void saveDownloadedFile(Drawable trailDraw)
+	private void saveDownloadedFile(Trail trail, Drawable trailDraw)
 			throws FileNotFoundException, IOException {
 		FileOutputStream fos;
-		fos = context.openFileOutput(String.valueOf(trail.getTrailId())
+		fos = getContext().openFileOutput(String.valueOf(trail.getTrailId())
 				+ "_120.png", Context.MODE_PRIVATE);
 		if (trailDraw != null) {
 			((BitmapDrawable) trailDraw).getBitmap().compress(
@@ -151,5 +148,57 @@ public abstract class TrailListAdapter extends ArrayAdapter<Trail> implements
 		}
 		fos.flush();
 		fos.close();
+	}
+	
+	public int getCount() {
+		return trails.size();
+	}
+	
+	@Override
+	public Trail getItem(int position) {
+		return trails.get(position);
+	}
+
+	public Filter getFilter() {
+		if (filter == null) {
+			filter = new TrailFilter();
+		}
+		return filter;
+	}
+
+	private class TrailFilter extends Filter {
+
+		protected FilterResults performFiltering(CharSequence constraint) {
+			FilterResults results = new FilterResults();
+			String filterString = constraint.toString().toLowerCase();
+
+			final ArrayList<Trail> items = Trail.getTrails();
+			final int count = items.size();
+			final ArrayList<Trail> newItems = new ArrayList<Trail>(count);
+			for (int i = 0; i < count; i++) {
+				final Trail item = items.get(i);
+				final String itemName = item.getName().toString().toLowerCase();
+				if (itemName.contains(filterString)) {
+					newItems.add(item);
+				}
+				results.values = newItems;
+				results.count = newItems.size();
+			}
+			return results;
+		}
+
+		@SuppressWarnings("unchecked")
+		@Override
+		protected void publishResults(CharSequence constraint,
+				FilterResults results) {
+			trails = (ArrayList<Trail>) results.values;
+			// Let the adapter know about the updated list
+			if (results.count > 0) {
+				notifyDataSetChanged();
+			} else {
+				notifyDataSetInvalidated();
+			}
+		}
+
 	}
 }
